88fa3b3121efdcdc5232300b0413915a3ca06b51
Iacopo Iacopini <iacopiniiacopo@gmail.com>
2021-02-02 17:42:34 +0100

    Add files via upload

--- Scripts/run_py_script_PARAMETERS_size_p.sh
+++ Scripts/run_py_script_PARAMETERS_size_p.sh
@@ -3,7 +3,7 @@
 for p in 0.03 0.08
 	do
 		for group_size in {2..20}	
-			do python test.py $group_size $p
+			do ../../../barrat/anaconda/bin/python SimHomMix_kuniformHG_PARAMETERS_size_p_I.py $group_size $p
 		done
 done
 
