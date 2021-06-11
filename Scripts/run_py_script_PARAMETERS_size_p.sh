f489ef46583bad2db24d949d7eb5f13c7f96395a
Iacopo Iacopini <iacopiniiacopo@gmail.com>
2021-06-11 15:41:44 +0200

    Delete Scripts directory

--- Scripts/run_py_script_PARAMETERS_size_p.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for p in 0.01 0.03 0.05 #0.08
-	do
-		for group_size in {21..40}	
-			do ../../../barrat/anaconda3/bin/python SimHomMix_kuniformHG_PARAMETERS_size_p_I.py $group_size $p &
-		done
-done
-
